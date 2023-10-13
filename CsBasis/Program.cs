81302415befe4ae9c2e8ca0728cb40712fb4c955
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-10-13 10:37:16 +0600

    Task 3.3.2

--- CsBasis/Program.cs
+++ CsBasis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 class MainClass
 {
@@ -11,9 +12,21 @@ class MainClass
         Console.WriteLine("\tПривет, мир");
         Console.WriteLine("\tМне 27 лет");
         Console.WriteLine($"\tMy name is\n {MyName}");
+        Console.WriteLine();
 
         Console.WriteLine("\u0040");
         Console.WriteLine("\u0023");
+        Console.WriteLine();
+
+        string name = "Vik";
+        byte age = 28;
+        bool havePet = true;
+        float shoeSize = 44.5f;
+
+        Console.WriteLine($"My name is {name}");
+        Console.WriteLine($"MyAge {age}");
+        Console.WriteLine($"Do I have a pet? {havePet}");
+        Console.WriteLine($"My shoe size is {shoeSize}");
 
         Console.ReadKey();
     }
